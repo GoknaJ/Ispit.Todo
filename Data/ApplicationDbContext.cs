54183d42ca1e1c0355ee001706d4dc0e6d29941f
GoknaJ <gakovski.janko@outlook.com>
2024-11-26 00:23:25 +0100

    Db

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Ispit.Todo.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ispit.Todo.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Todolist> Todolists { get; set; } 
+        public DbSet<TodoTask> TodoTasks { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
